dc84364cefcb890f3f0e870551a90429a9a37d65
Faysal Regal <faysalregal@gmail.com>
2022-10-08 21:10:05 +0100

    first commit

---
+++ Csharp-Intermediate-S2-Exercise-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Intermediate_S2_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
